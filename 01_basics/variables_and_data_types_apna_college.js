2d00fa3f32249df4fe078f2044e0e828f12562d7
Niiraj028 <nirajkumar01007@gmail.com>
2025-01-21 09:17:46 +0000

    done

---
+++ 01_basics/variables_and_data_types_apna_college.js
@@ -0,0 +1,19 @@
+const product = {
+    Title: "Ball Pen",
+    Rating: 4,
+    Offer: 5,
+    Price: 270
+};
+console.log(product);
+
+const profile = {
+    UserName: "Niraj Sharma",
+    NumberOfPost: 3,
+    Followers: 234,
+    Following: 245
+};
+
+console.log(profile);
+console.log(typeof profile)
+console.log(typeof profile["UserName"])
+console.log(typeof profile["Followers"])
