4973a0c546197c2284b85fa322621a848852bdd1
Niiraj028 <nirajkumar01007@gmail.com>
2025-01-22 09:47:37 +0000

    done

---
+++ 01_basics/maths.js
@@ -0,0 +1,7 @@
+// console.log(Math.random());
+// console.log(Math.round((Math.random() * 10)));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
